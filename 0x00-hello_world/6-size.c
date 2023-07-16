23588bcc64cfca45426b35087bc81fa051d11620
Alabi Ololade Maiam <mariamalabi44@.com>
2023-07-15 21:31:39 -0700

    6-size.c1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,12 @@
  */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
+	char a;
+        int b;
+        long int c;
+        long long int d;
+        float f;
+
 printf("Size of a char: %1u byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %1u byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %1u bytes(s)\n" (unsigned long)sizeof(c));
